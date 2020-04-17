209e4283176970b9aa52dc77933f3ed91f5307d4
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-04-16 20:53:12 -0700

    Setup Completed

---
+++ Week_02/week2_php.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<P>The following code was created by PHP</P>
+
+<?php
+// This is a comment
+/* This is a 
+multiline comment */
+    print "<strong class=\"bold\">Hellow \n  There!</strong>";
+
+    ?>
+    
+</body>
+</html>
